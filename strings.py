719f60c4e5fb75ff073807e2fc0e20e5f1154af0
unknown <rmtestard@gmail.com>
2019-04-10 16:17:24 +0200

    strings

---
+++ strings.py
@@ -0,0 +1,15 @@
+print('Hello\nWorld')
+
+print('Hello\"World')
+
+phrase = 'Hello World'
+print(phrase)
+print(phrase + ' is cool')
+
+print(phrase.upper())  # .lower()
+print(phrase.upper().isupper())
+print(len(phrase))
+print(phrase[3])
+print(phrase.index('H'))
+print(phrase.index('Hello'))
+print(phrase.replace('World', 'Banana'))
