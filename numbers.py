dac0afebe794b41047768ea60d3e36783ced6788
unknown <rmtestard@gmail.com>
2019-04-10 18:50:47 +0200

    numbers

---
+++ numbers.py
@@ -0,0 +1,35 @@
+from math import *
+
+x = 10
+y = 2.23
+
+sum = x + y
+remainder = x % y
+
+print(sum)
+print(remainder)
+
+# convert to string
+print(str(remainder) + ' my favorite number')
+
+# absolute values
+z = -5
+print(abs(z))
+
+# power of a number, 3 power of 2
+print(pow(3, 2))
+
+# max value // works the same with min
+print(max(4, 6))
+
+# round a number
+print(round(y))
+
+# floor, takes the lowest closest round number
+print(floor(2.23))
+
+# ceil, takes the highest closest round number
+print(ceil(y))
+
+# square root
+print(sqrt(36))
