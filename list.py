04e82212e314bb46d3c09f655284d8cab08f4bb2
unknown <rmtestard@gmail.com>
2019-04-12 14:03:18 +0200

    list

---
+++ list.py
@@ -0,0 +1,6 @@
+friends = ['Roger', 'Kevin', 'Lucie', 'Oscar']
+friends[1] = 'Romain'
+
+print(friends[0])
+print(friends[-1])  # backward
+print(friends[1:3])  # up to but not including
--- madlibs.py
+++ madlibs.py
@@ -2,7 +2,6 @@ color = input('Enter a color: ')
 plural_noun = input('Enter a plural noun: ')
 celebrity = input('Enter a celebrity: ')
 
-
 print(f'Roses are {color}')
 print(f'{plural_noun} are blue')
 print(f'I love {celebrity}')
